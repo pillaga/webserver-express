26a6d72c9b2fc949293ddb3ed2c3cdb4ff0a6c46
Jorge Pillaga <pillaga@MacBook-Pro-de-Jorge.local>
2019-01-16 07:11:31 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+const hbs=require('hbs');
+require('./hbs/helpers');
+
+ 
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+const port= process.env.PORT || 3000;
+
+app.get('/', (req, res)=> {
+ 
+  res.render('home',{
+      nombre:'Jorge'
+  });
+
+});
+
+app.get('/about', (req, res)=> {
+ 
+    res.render('about',{});
+  
+  });
+
+
+ 
+app.listen(port,()=>{
+        console.log(`escuchando en el puertooo${port}`);
+});
